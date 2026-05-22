1d880c77b82231205c646ddef5b1b6386c1d2731
thomas <thomas@kngstn.eu>
2026-05-22 17:30:07 +0200

    Add azure storage as backend

--- providers.tf
+++ providers.tf
@@ -4,11 +4,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    
+
     time = {
       source  = "hashicorp/time"
       version = "~> 0.9.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-internal" 
+    storage_account_name = "tfstate4711"
+    container_name       = "tfstate"
+    key                  = "azure-tf.terraform.tfstate"
   }
 }
 
